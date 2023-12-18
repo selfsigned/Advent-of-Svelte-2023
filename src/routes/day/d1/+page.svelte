27b8d5a4fa4011b62adc5afe0f13d678a6a26981
Selfsigned <me@selfsigned.dev>
2023-12-18 21:25:59 +0100

    Dynamic data sorting done

--- src/routes/day/d1/+page.svelte
+++ src/routes/day/d1/+page.svelte
@@ -1,6 +1,15 @@
 <script>
-	import { base } from '$app/paths';
 	export let data;
+
+	let nameSearch = '';
+	const origTally = data.tally;
+	let tally = origTally;
+
+	$: if (nameSearch.length > 0) {
+		tally = origTally.filter((kid) => kid.name.toLowerCase().includes(nameSearch.toLowerCase()));
+	} else {
+		tally = origTally;
+	}
 </script>
 
 <svelte:head>
@@ -12,19 +21,31 @@
 		<div class="m-5 self-center">
 			<h1 class="text-5xl">Naughty or Nice</h1>
 		</div>
+		<input
+			class="input-bordered input-lg max-w-xs self-center"
+			placeholder="Check if they've been naughty"
+			type="text"
+			bind:value={nameSearch}
+		/>
 		<div class="m-10 grow self-center overflow-x-auto">
 			<div class="table">
 				<thead>
 					<tr>
 						<th>Name</th>
 						<th>Tally</th>
+						<th>Nice</th>
 					</tr>
 				</thead>
 				<tbody>
-					{#each data.tally as kid}
+					{#each tally as kid}
 						<tr>
 							<th>{kid.name}</th>
 							<th>{kid.tally}</th>
+							{#if kid.tally > 0}
+								<th class="badge badge-success">Nice</th>
+							{:else}
+								<th class="badge badge-error">Naughty</th>
+							{/if}
 						</tr>
 					{/each}
 				</tbody>
