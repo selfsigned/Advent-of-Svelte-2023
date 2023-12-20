79e524427f453cf9f35f0a8c1b676bcb87bc0e90
Xavier Perrin <me@selfsigned.dev>
2023-12-20 05:29:12 +0100

    Fix drawers / navbar (#3)
    * Fix drawer
    
    * Fix theme selector z-value
    
    * Improvements to Day 1
    
    * Add README.md

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,17 +1,14 @@
 <script>
 	import ThemeSwitcher from './themeswitcher.svelte';
-	import { range } from '$lib/js/range.js';
 	import { onMount } from 'svelte';
 	import { base } from '$app/paths';
 
-	const repoURL = 'https://github.com/selfsigned/Advent-of-Svelte-2023';
-	const challengeURL = 'https://advent.sveltesociety.dev';
-
-	export let routes;
-	export let prefix = '';
+	export let repoURL = '';
+	export let challengeURL = '';
 	export let themeswitcher = true;
-	let theme_support = false;
+	export let drawer = false;
 
+	let theme_support = false;
 	onMount(async () => {
 		if (browser) {
 			theme_support = CSS.supports('selector(:has(*)');
@@ -19,58 +16,28 @@
 	});
 </script>
 
-<div class="navbar bg-base-100 bg-opacity-90 shadow-sm backdrop-blur transition-shadow">
+<div class="bg-base-100 bg-opacity-90 shadow-sm backdrop-blur transition-shadow">
 	<nav class="navbar">
 		<div class="flex-1">
-			<div class="drawer w-auto">
-				<input id="menu-drawer" type="checkbox" class="drawer-toggle" />
-				<div class="drawer-content">
-					<label for="menu-drawer" class="btn btn-ghost drawer-button">
-						<!-- Sidebar Menu Content -->
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							class="inline-block h-5 w-5 stroke-current"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h16M4 18h16"
-							></path></svg
-						>
-					</label>
-				</div>
-				<div class="drawer-side">
-					<label for="menu-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-					<ul class="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-						<!-- Sidebar content here -->
-						<li><a href="{base}/">Home</a></li>
-						<li>
-							<h2 class="menu-title">Web</h2>
-							<ul>
-								<li>
-									<a target="_blank" rel="noopener" href={challengeURL}>Challenges</a>
-								</li>
-								<li>
-									<a target="_blank" rel="noopener" href={repoURL}>Source code</a>
-								</li>
-							</ul>
-							<h2 class="menu-title">Days</h2>
-							<ul>
-								{#each range(1, 24) as dayNbr}
-									{#if dayNbr in routes}
-										<li><a href={routes[dayNbr]}>{prefix}{dayNbr}</a></li>
-									{/if}
-								{/each}
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<a href="{base}/" class="btn btn-ghost w-auto text-lg">
-				<img class="w-10" src="{base}/favicon.png" alt="Advent of Svelte logo" />
-				<span class="invisible sm:visible">Advent of Svelte</span>
+			{#if typeof drawer === 'string' && drawer.length > 0}
+				<label for="menu-drawer" class="btn btn-ghost drawer-button">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-5 w-5 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path></svg
+					>
+				</label>
+			{/if}
+			<a href={base + '/'} class="btn btn-ghost hidden w-auto text-lg sm:flex">
+				<img class="w-8" src={base + '/favicon.png'} alt="Advent of Svelte logo" />
+				<span class="hidden md:flex">Advent of Svelte</span>
 			</a>
 		</div>
 		<div>
@@ -78,7 +45,7 @@
 				target="_blank"
 				rel="noopener"
 				href={challengeURL}
-				class="btn btn-ghost w-auto hover:rotate-90">WTF?</a
+				class="btn btn-ghost hidden w-auto hover:rotate-180 sm:flex">WTF?</a
 			>
 			<a target="_blank" rel="noopener" href={repoURL} class="btn btn-ghost w-auto">
 				<svg class="w-7 fill-base-content" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"
